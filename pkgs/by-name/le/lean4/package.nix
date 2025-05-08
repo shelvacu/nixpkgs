a5dad1fa42bdc61c8d9520bdbf2b84ac67d51a2e
Shelvacu <git@shelvacu.com>
2025-05-08 11:21:28 -0700

    lean4: 4.18.0 -> 4.19.0
    This now needs mimalloc source, but that shouldn't affect consumers of the build
    
    Changelog: https://lean-lang.org/doc/reference/latest/releases/v4.19.0/#release-v4___19___0

--- pkgs/by-name/le/lean4/package.nix
+++ pkgs/by-name/le/lean4/package.nix
@@ -11,25 +11,54 @@
   perl,
   testers,
 }:
-
+# lean wants to grab a copy of mimalloc's source with git and compile it along with lean, we patch it to do nothing and put the mimalloc source where it would've been downloaded to
 stdenv.mkDerivation (finalAttrs: {
   pname = "lean4";
-  version = "4.18.0";
+  version = "4.19.0";
 
-  src = fetchFromGitHub {
+  mainSrc = fetchFromGitHub {
+    name = "lean";
     owner = "leanprover";
     repo = "lean4";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-1hVcRO9RbVUgoKTUTFXBqJZwt50/aw/P9dxUdI7RpCc=";
+    hash = "sha256-Iw5JSamrty9l6aJ2WwslAolSHfi2q0UO8P8HI1gp+j8=";
   };
+
+  # From CMakeLists.txt in mainSrc
+  mimallocVersion = "2.2.3";
+
+  mimallocSrc = fetchFromGitHub {
+    name = "mimalloc";
+    owner = "microsoft";
+    repo = "mimalloc";
+    tag = "v${finalAttrs.mimallocVersion}";
+    hash = "sha256-B0gngv16WFLBtrtG5NqA2m5e95bYVcQraeITcOX9A74=";
+  };
+
+  srcs = [
+    finalAttrs.mainSrc
+    finalAttrs.mimallocSrc
+  ];
+
+  patches = [
+    ./no-download.patch
+  ];
 
   postPatch = ''
     substituteInPlace src/CMakeLists.txt \
-      --replace-fail 'set(GIT_SHA1 "")' 'set(GIT_SHA1 "${finalAttrs.src.tag}")'
+      --replace-fail 'set(GIT_SHA1 "")' 'set(GIT_SHA1 "${finalAttrs.mainSrc.tag}")'
 
     # Remove tests that fails in sandbox.
     # It expects `sourceRoot` to be a git repository.
     rm -rf src/lake/examples/git/
+  '';
+
+  sourceRoot = ".";
+
+  postUnpack = ''
+    mkdir -p lean/build/mimalloc/src
+    mv mimalloc lean/build/mimalloc/src
+    cd lean
   '';
 
   preConfigure = ''
@@ -67,7 +96,7 @@ stdenv.mkDerivation (finalAttrs: {
   meta = with lib; {
     description = "Automatic and interactive theorem prover";
     homepage = "https://leanprover.github.io/";
-    changelog = "https://github.com/leanprover/lean4/blob/${finalAttrs.src.tag}/RELEASES.md";
+    changelog = "https://github.com/leanprover/lean4/blob/${finalAttrs.mainSrc.tag}/RELEASES.md";
     license = licenses.asl20;
     platforms = platforms.all;
     maintainers = with maintainers; [
