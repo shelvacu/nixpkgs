931f1ee271125d2b74e49be8cf8afffc957f2acf
Shelvacu <git@shelvacu.com>
2026-03-16 15:53:27 -0700

    wip

--- pkgs/by-name/sc/scoop/package.nix
+++ pkgs/by-name/sc/scoop/package.nix
@@ -1,12 +1,14 @@
 {
   # eval deps
   buildNpmPackage,
+  fetchpatch,
   fetchFromGitHub,
   lib,
   nix-update-script,
 
   # runtime deps
   certificate-ripper,
+  nodejs_20,
   playwright-driver,
   playwright-test,
   yt-dlp,
@@ -21,6 +23,14 @@ buildNpmPackage (finalAttrs: {
     tag = finalAttrs.version;
     hash = "sha256-KcoTl2ehla6ALLrpAyfOqDioP5s3CGVfVhsj3Fbq6/Y=";
   };
+
+  patches = [ (fetchpatch {
+    url = "https://github.com/harvard-lil/scoop/pull/414/commits/4dc4ee6d4e6c3fa89cf5ec4c685178f1927ffa1f.patch";
+    hash = "sha256-zV7axS3zlD/hWwgZHTOkMLVak71giWy+J0iwZ3eSKL4=";
+  }) ];
+
+  # scoop doesn't work on node 22+ https://github.com/harvard-lil/portal/issues/12
+  nodejs = nodejs_20;
 
   npmDepsHash = "sha256-lx+MxV8JDArDMQ0OV4eDXOTzd7xRfvvpAGJp+jPQPgs=";
 
